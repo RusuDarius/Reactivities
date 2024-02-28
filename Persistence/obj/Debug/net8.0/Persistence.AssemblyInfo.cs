e7c67e44788879ba6c6293cc0c3730a93e228d27
Rusu Darius <dariusrusu7@gmail.com>
2024-02-28 14:23:33 +0200

    Added Activity interface client-side & solved any type issue

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a987f88d579977aea58883cb8f3c1289b49239d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2731895c7467c2104130af8a98af55c2fbcf988")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
