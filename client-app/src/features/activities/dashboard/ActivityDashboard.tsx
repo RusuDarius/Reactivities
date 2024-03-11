4086460d09db84c417173882b83a74a030c15938
Rusu Darius <dariusrusu7@gmail.com>
2024-03-11 14:27:52 +0200

    Added router outlet to App and handled potential wasted API calls

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,10 +4,19 @@ import ActivityDetails from "../Details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponents from "../../../app/layout/LoadingComponents";
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore();
   const { selectedActivity, isFormOpen } = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponents content="Loading app..." />;
 
   return (
     <Grid>
