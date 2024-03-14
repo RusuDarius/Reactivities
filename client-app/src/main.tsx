d3da6427d45a2e39b996abfca341817f9c927c62
Rusu Darius <dariusrusu7@gmail.com>
2024-03-14 21:19:15 +0200

    Renamed router config file

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,7 +5,7 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import { StoreContext, store } from "./app/stores/store.ts";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router/route.tsx";
+import { router } from "./app/router/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
