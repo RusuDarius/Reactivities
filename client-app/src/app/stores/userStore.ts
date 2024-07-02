99c7ac2a2af500d46bc896196491b5e4f16838a7
Rusu Darius <dariusrusu7@gmail.com>
2024-07-03 00:17:43 +0300

    Persistance the login after refresh

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -24,8 +24,16 @@ export default class userStore {
 
   logout = () => {
     store.commonStore.setToken(null);
-    localStorage.removeItem("jwt");
     this.user = null;
     router.navigate("/");
+  };
+
+  getUser = async () => {
+    try {
+      const user = await agent.Account.current();
+      runInAction(() => (this.user = user));
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
